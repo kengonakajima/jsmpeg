257493ed004eee4ed8668393921ea8c4ad1961be
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-11-27 09:14:55 +0900

    mod

---
+++ cat.sh
@@ -0,0 +1,26 @@
+cat \
+	src/jsmpeg.js \
+	src/video-element.js \
+	src/player.js \
+	src/buffer.js \
+	src/ajax.js \
+	src/fetch.js \
+	src/ajax-progressive.js \
+	src/websocket.js \
+	src/ts.js \
+	src/decoder.js \
+	src/mpeg1.js \
+	src/mpeg1-wasm.js \
+	src/mp2.js \
+	src/mp2-wasm.js \
+	src/webgl.js \
+	src/canvas2d.js \
+	src/webaudio.js \
+	src/wasm-module.js \
+    jsmpeg.wasm.inline.inc.js \
+	> hoge.js
+
+
+
+
+
